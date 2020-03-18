3f3c84ce86b09736e27f5bfc7660280d6b3bddf3
Ian McCulloch <ianmcculloch@protonmail.com>
2020-03-18 20:27:17 +0100

    updated download-hls.sh to my version of the code and added downVid.config

--- download-hls.sh
+++ download-hls.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+. downVid.config
+dir="$downPath"
+echo "$dir"
+#if [ -z $1 ]
+#then
+#    echo "usage: download-hls URL [name to save]"
+#    exit 1
+#fi
 
-if [ -z $1 ] 
-then
-    echo "usage: download-hls URL [name to save]"
-    exit 1
-fi
+#Filename with .ts ending
+echo "Welcome to the hls-downloader. Enjoy!"
+echo "Filename:"
+filename_raw=$2
+filename="$filename_raw.ts"
+#Original filename was the second argument
+#filename=${2:-"save"}.ts
 
-filename=${2:-"save"}.ts
-
+#Check if filename exists.
 if [ -f "$filename" ]
 then
     echo "File ${filename} already exists!"
@@ -16,8 +25,11 @@ fi
 
 echo "Save file to $filename"
 
+
 status="begin"
 count=1
+#echo "URL:"
+#url= read
 url="$1"
 curl "$url" > temp.m3u8
 cat temp.m3u8 | \
@@ -27,7 +39,7 @@ while read line; do
         status="reading"
         continue
     fi
-    
+
     if [[ $status == "reading" ]]
     then
         curl -s --show-error "${line}" >> "$filename"
@@ -36,7 +48,7 @@ while read line; do
         let "count += 1"
         continue
     fi
-    
+
     if [[ $line == "#EXT-X-ENDLIST" ]]
     then
         status="done"
@@ -45,3 +57,8 @@ while read line; do
         exit 0
     fi
 done
+
+echo "Filename: $filename"
+ffmpeg -i $filename -c copy -bsf:a aac_adtstoasc "$filename_raw.mp4"
+rm $filename
+mv "$filename_raw.mp4" $dir
